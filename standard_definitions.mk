0e0658c0c6df9d7a46b83ede71c9f92ac4884dff
Isaac Dunham <idunham@lavabit.com>
2013-03-23 17:27:59 +0000

    Start cleaning up the build system, step 1.
    -standard_definitions.mk: Use PREDIR more
    -libtinyx11/Makefile: wrap a long line of flags

--- standard_definitions.mk
+++ standard_definitions.mk
@@ -1,7 +1,7 @@
 
 #compiler
-#CC=/usr/bin/gcc
-CC=/opt/musl/bin/musl-gcc
+CC=/usr/bin/gcc
+#CC=/opt/musl/bin/musl-gcc
 
 #compilerflags
 COMPFLAGS=-pipe -Os -mtune=i386 -Wall -D_XOPEN_SOURCE=600 -D_BSD_SOURCE -D_GNU_SOURCE -fno-strength-reduce -nodefaultlibs -fno-strict-aliasing  -I. -ffunction-sections -fdata-sections
@@ -10,12 +10,15 @@ COMMONDEFS=$(COMPFLAGS) -D_BSD_SOURCE -D_GNU_SOURCE
 
 LDFLAGS=-static -Wl,--gc-sections,--sort-common,-s
 
-LINKDIR=-L/opt/musl/lib
-
-LIBDIR=/opt/musl/lib
-
-INCDIR=/opt/musl/include
+#LINKDIR=-L/opt/musl/lib
+LINKDIR=-L/usr/lib
 
 PREDIR=/opt/musl
+
+LIBDIR=${PREDIR}/lib
+
+INCDIR=${PREDIR}/include
+
+
 
 
