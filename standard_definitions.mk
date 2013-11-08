824e047a0631a0f7c91d119a516ab6589e39e55a
Dima Krasner <dima@dimakrasner.com>
2013-11-08 20:12:20 +0000

    Removed hardcoded configuration values and remaining shared libraries.

--- standard_definitions.mk
+++ standard_definitions.mk
@@ -1,18 +1,13 @@
+CC ?= cc
+EXTRA_CFLAGS ?= -pipe -Os -Wall
+COMPFLAGS = $(EXTRA_CFLAGS) -D_XOPEN_SOURCE=600 -D_BSD_SOURCE -D_GNU_SOURCE -I.
 
-#compiler
-CC=/mnt/sda6/Stuff/git/lazyux/sysroot/bin/musl-gcc
+COMMONDEFS = $(COMPFLAGS) -D_BSD_SOURCE -D_GNU_SOURCE
 
-#compilerflags
-COMPFLAGS=-pipe -fPIC -Os -Wall -D_XOPEN_SOURCE=600 -D_BSD_SOURCE -D_GNU_SOURCE -fno-strength-reduce -nodefaultlibs -fno-strict-aliasing  -I. -ffunction-sections -fdata-sections
+LDFLAGS ?= -s
 
-COMMONDEFS=$(COMPFLAGS) -D_BSD_SOURCE -D_GNU_SOURCE
+LIBDIR ?= /usr/lib
 
-LDFLAGS=-Wl,--gc-sections,--sort-common,-s
+INCDIR ?= /usr/include
 
-LINKDIR=-L/mnt/sda6/Stuff/git/lazyux/sysroot/lib
-
-LIBDIR=/mnt/sda6/Stuff/git/lazyux/sysroot/lib
-
-INCDIR=/mnt/sda6/Stuff/git/lazyux/sysroot/usr/include
-
-PREDIR=/mnt/sda6/Stuff/git/lazyux/sysroot/lib
+PREDIR ?= /usr
